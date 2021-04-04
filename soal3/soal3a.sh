0f5edbec751cc3505075fa4a132b0b39bbadeb39
Alvancho88 <61174498+Alvancho88@users.noreply.github.com>
2021-04-04 12:16:32 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,8 +1,32 @@
-#! /bin/bash
+#!/bin/bash
+
+dirloc=/home/alvancho/Documents/IO5/Soal3
+
+if [ -f $dirloc/Foto.log ]
+then
+	rm $dirloc/Foto.log
+fi
 
 for ((counter=1; counter<=23; counter=counter+1))
-do
-	wget -a Foto.log -O Koleksi_$counter  https://loremflickr.com/320/240/kitten
-	fdupes -dN /home/alvancho
+    do
+    if [ $counter -lt 10 ]
+        then wget -a "$dirloc"/Foto.log "https://loremflickr.com/320/240/kitten" -O "$dirloc"/Koleksi_0"$counter".jpg
+    else wget -a "$dirloc"/Foto.log "https://loremflickr.com/320/240/kitten" -O "$dirloc"/Koleksi_"$counter".jpg
+    fi
+done
 
+
+counter=1
+cd "$dirloc"
+fdupes -dN "$dirloc"
+
+for f in Koleksi_*.jpg
+    do
+    if [ $counter -lt 10 ]
+        then 
+            mv -- "$f" "Koleksi_0$counter.jpg"
+    else 
+        mv -- "$f" "Koleksi_$counter.jpg"
+    fi
+let counter=$counter+1
 done
